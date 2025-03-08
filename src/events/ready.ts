7654928f092c8849a46a216ba068928cb6a11b01
Samarth Sharma <110784602+bugsum@users.noreply.github.com>
2025-03-08 22:28:05 +0530

    Added base status on the bot

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,16 @@
 import { createEvent } from "@handlers/eventHandler";
 import { logger } from "@lib/utils";
+import { ActivityType } from "discord.js";
 
 export default createEvent({
     name: "ready",
     runOnce: true,
     listener: (client) => {
         logger.info(`${client.user?.username} is ready!`);
+
+        client.user?.setActivity({
+            name: "to Endbyte",
+            type: ActivityType.Listening,
+        });
     },
 });
